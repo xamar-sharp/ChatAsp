5ffcbbced42a7a452928e2031921f1c8a0852552
xamar-sharp <xamacoredevelopment@gmail.com>
2022-11-07 23:20:42 +0300

    Добавьте файлы проекта.

---
+++ ChatAsp/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+using System.Text;
+namespace ChatAsp
+{
+    public class ChatHub : Hub
+    {
+        public async Task GetDataFromClient(string text)
+        {
+            await Task.Yield();
+            throw new System.Exception(text);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("GetImage", await System.IO.File.ReadAllBytesAsync("C:\\solidcolorbrushes.png"));
+            await base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
